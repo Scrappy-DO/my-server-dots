5285f92da98cfc427bb29cad9fa65708296246be
root <root@alpine.vdv>
2024-01-21 12:32:21 +0100

    nvchad

--- alpine.sh
+++ alpine.sh
@@ -7,6 +7,6 @@ else
 apk add lsd zellij bat fish starship git wget curl neovim
 fi
 
-
 mkdir -p ~/.config
 cp -r ./configs/* ~/.config/
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
